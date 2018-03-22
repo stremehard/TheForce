39c42ac9a0f8fe9b291880226da0cc4664ea917a
Yoda <eocm1997@gmail.com>
2018-03-22 18:54:17 +0200

    Exercises on Atmega2560 using Assembler language

---
+++ ExercisesAtmega2560/ExercisesAtmega2560/main.asm
@@ -0,0 +1,12 @@
+;
+; ExercisesAtmega2560.asm
+;
+; Created: 3/22/2018 5:16:41 PM
+; Author : StremeHard
+;
+
+
+; Replace with your application code
+
+	ldi r16, 0b0100_0010	;load value into register r16	
+	nop		;does nothing, but provides a place to put a breakpoint
